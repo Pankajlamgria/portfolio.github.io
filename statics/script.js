ac98533b848deff76bbc08995807911471f747be
Pankajlamgria <125663310+Pankajlamgria@users.noreply.github.com>
2023-02-19 20:48:54 +0530

    Delete statics directory

--- statics/script.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/mattboldt/typed.js/ form here first add the script link
-var typed =new Typed(".multiple-text",{
-    strings:["Web Developer","Programer","Coder"],
-    typeSpeed:90,
-    backSpeed:90,
-    backDelay:1000,
-    loop:true
-})
